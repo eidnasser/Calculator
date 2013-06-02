adea6fe1a878c3f0aa07fbd9a9a2e467ce44ee31
eidnasser <alkhalaf.eid@gmail.com>
2013-06-02 13:36:54 +0300

    assignment2 issue

--- Calculatorbrain.m
+++ Calculatorbrain.m
@@ -10,9 +10,12 @@
 
 @interface  Calculatorbrain()
 @property (nonatomic,strong) NSMutableArray *programStack;
+@property(nonatomic,strong) NSMutableDictionary *varDictionary;
+
 @end
 
 @implementation Calculatorbrain
+@synthesize varDictionary=_varDictionary;
 @synthesize programStack = _programStack;
 
 -(NSMutableArray *)operandStack
@@ -22,6 +25,15 @@
     
 }
     return _programStack;
+}
+
+
+
+-(void) setDictionaryOfValues:(NSDictionary *) varDict
+{
+    NSMutableDictionary *dict=[[NSMutableDictionary alloc] initWithObjectsAndKeys:@"3",@"a",@"5",@"b",@"9",@"x", nil];
+    
+    _varDictionary=dict;
 }
 
 -(void) setOperandStack:(NSMutableArray *)anArray{
@@ -36,8 +48,8 @@
 
 -(void)pushOperant:(double)operand
 {
-       NSNumber *operandObject=[NSNumber numberWithDouble:operand];
-    NSLog(@"contents %@",self.operandStack);
+    NSNumber *operandObject=[NSNumber numberWithDouble:operand];
+     //NSLog(@"contents %@",self.operandStack);
     [self.programStack addObject:operandObject];
     
 }
@@ -48,15 +60,116 @@
     [self.programStack addObject:opration];
     return [Calculatorbrain runProgram:self.program];
 }
+
 -(id)program
 {
     return [self.programStack copy];
 }
+
 +(NSString *)descriptionOfProgram:(id)program
 {
   // here we write the description of the program
 }
 
+
++(BOOL) isOperation:(id)operationName
+{
+    NSSet *operationSet =[NSSet setWithObjects:@"+",@"*",@"/",@"-",@"Sin",@"Cos",@"Sqrt",@"Log", nil ];
+    if([operationSet containsObject:operationName])
+    {
+        return YES;
+    }
+    else
+    {
+        return NO ;
+    }
+}
+
++(NSNumber *) valueForVariable:(NSString *) nameOfVariable from:(NSDictionary *)variableValues
+{
+    
+    id result=0;
+    
+    if([nameOfVariable isEqualToString:@"a"])
+    {
+        result=[variableValues valueForKey:@"a"];
+        return result;
+    }
+    else if ([nameOfVariable isEqualToString:@"b"])
+    {
+        result=[variableValues valueForKey:@"b"];
+        return result;
+    }
+    else if ([nameOfVariable isEqualToString:@"x"])
+    {
+        result=[variableValues valueForKey:@"x"];
+        return result;
+    }
+    else
+    {
+        return 0;
+    }
+    
+    
+    
+    
+    //1-find the variable name in the dictionary
+    //2- if the variable name is found return the nsnumber object
+    //3-else return nsnumber with value zero
+}
+
++(void) replaceProgram:(id) program variable:(NSString *) nameofVariable withValue:(NSNumber *) value
+{
+    NSMutableArray *stack1;
+    if([program isKindOfClass:[NSArray class]])
+    {
+        stack1=[program mutableCopy];
+    }
+    
+    
+    for(NSObject *item in program)
+    {
+        if ([item isKindOfClass::[NSNumber Class]])
+        {
+            [stack1 addObject:item];
+            
+        }
+        else
+        {
+            if([Calculatorbrain isOperation:item]){
+                [stack1 addObject:item];
+            }
+            else
+            {
+                [stack1 addObject:[Calculatorbrain valueForVariable ]];
+            
+            }
+        
+        }
+        
+        
+        
+        // if (![Calculatorbrain isOperation:item]) {
+         //   if (![item isKindOfClass:[NSNumber class]]) {
+           //     [chkVar addObject:item];
+            //}
+        //}
+    }
+    
+    
+    //-go over the program and if the variable name is found replace it with value provided
+}
+
+-(void) pushVaiable:(NSString *) varaible
+{
+    [self.programStack addObject:varaible];
+    
+}
+
+-(void) pushOperandToStack:(NSMutableArray *)stack
+{
+
+}
 +(double) popOperandOfStack:(NSMutableArray *)stack
 {
     double result=0;
@@ -108,6 +221,26 @@
     
     return result;
 }
++(NSSet *)variableUsedInProgram:(id)program
+{
+    NSMutableSet *chkVar=[[NSMutableSet alloc]init];
+    for(NSObject *item in program)
+    {
+        if (![Calculatorbrain isOperation:item]) {
+            if (![item isKindOfClass:[NSNumber class]]) {
+                [chkVar addObject:item];
+            }
+        }
+    }
+    
+    if ([chkVar count]>0) {
+        return [chkVar copy];
+        
+    }
+    else {
+        return nil;
+    }
+}
 
 +(double) runProgram:(id)program
 {
@@ -120,4 +253,4 @@
     
 }
 
-    @end
+@end
